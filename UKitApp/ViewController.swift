318077c724e78ef8309320de24223ac55fa6ee79
AlexeyB163 <sourceaisa@mail.ru>
2021-12-26 15:02:44 +0400

    adding UIKit element

--- UKitApp/ViewController.swift
+++ UKitApp/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Label
+        mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        //Segmented Control
+        segmentedControl.insertSegment(withTitle: "Three", at: 2, animated: false)
+        
+        //Slider
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .cyan
+        mainLabel.text = String(slider.value)
     }
-
-
+    
+    @IBAction func segmentedControlAction() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .cyan
+        default:
+            mainLabel.text = "The three segment is selected"
+            mainLabel.textColor = .yellow
+        }
+    }
+    
+    @IBAction func sliderAction() {
+        mainLabel.text = String(slider.value)
+        let sliderValue = CGFloat(slider.value)
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(sliderValue)
+        
+    }
 }
 
